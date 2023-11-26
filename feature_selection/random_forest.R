99b2efe65cee800b35d41590d36a319c60b45f00
WonkyVamp <navya99gpt@gmail.com>
2023-11-26 16:40:26 -0600

    updates

--- feature_selection/random_forest.R
+++ feature_selection/random_forest.R
@@ -16,9 +16,9 @@ print(nlevels(data$project_id))
 if (nlevels(data$project_id) >= 2) {
   features <- data[,2:(ncol(data)-1)]
   project_id <- data$project_id
-    rf_model <- randomForest(features, project_id, ntree=100, importance=TRUE)
+  rf_model <- randomForest(features, project_id, ntree=100, importance=TRUE)
   importance_scores <- importance(rf_model)[,"MeanDecreaseGini"]
-    feature_names <- colnames(features)
+  feature_names <- colnames(features)
   
   if (length(feature_names) == length(importance_scores)) {
     feature_importance <- data.frame(Feature = feature_names, Importance = importance_scores)
@@ -26,6 +26,11 @@ if (nlevels(data$project_id) >= 2) {
     ordered_features <- feature_importance %>% arrange(desc(Importance))
     
     top_features <- head(ordered_features, 50)
+    top_feature_names <- top_features$Feature
+    selected_data <- data[, c(top_feature_names, "project_id")]
+    
+    # Save the selected data to a new CSV file
+    write.csv(selected_data, "top_features_data.csv", row.names = FALSE)
     
     print(top_features)
   } 
